acbb037f0e533edca9237140fd56849d60ad4eb6
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-10 15:05:58 -0300

    Scaffold structure, still in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-main(List<String> args) {
-  runApp(new PerguntaApp());
-}
+main(List<String> args) => runApp(new PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello Flutter 2!'));
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Questions'),
+          ),
+          body: Text('Welcome Flutter 2!')),
+    );
   }
 }
