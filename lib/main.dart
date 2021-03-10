a6c407f7c145e4a48332926f8e8e5c2533e94da5
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-10 15:27:38 -0300

    Buttons created and print func being called

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,29 @@ import 'package:flutter/material.dart';
 main(List<String> args) => runApp(new PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  void answer() {
+    print('Question answered');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<String> questions = [
+      'Which is your favorite color?',
+      'Which is your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
             title: Text('Questions'),
           ),
-          body: Text('Welcome Flutter 2!')),
+          body: Column(
+            children: [
+              Text(questions[0]),
+              ElevatedButton(onPressed: answer, child: Text('Response 1')),
+              ElevatedButton(onPressed: answer, child: Text('Response 2')),
+              ElevatedButton(onPressed: answer, child: Text('Response 3'))
+            ],
+          )),
     );
   }
 }
