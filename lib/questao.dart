e453e136ceb3083fc3e3740296200fd0ec257fc7
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-11 20:27:35 -0300

    class intialy defined

--- lib/questao.dart
+++ lib/questao.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Questao extends StatelessWidget {
+  final text;
+
+  Questao(this.text);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Text(text);
   }
 }
