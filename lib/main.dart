3fd65a8ee7397fbf830a5b897aab0988d75c996a
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-15 20:28:34 -0300

    now method isThereSelectedQuestion checks if there is a next question or not

--- lib/main.dart
+++ lib/main.dart
@@ -9,46 +9,55 @@ main(List<String> args) => runApp(new PerguntaApp());
 class _PerguntaAppState extends State<PerguntaApp> {
   //classe para gerenciar estado
   var _questionSelected = 0;
+  final List<Map<String, Object>> _questions = const [
+    {
+      'text': 'Which is your favorite color?',
+      'answer': ['Red', 'Green', 'Blue', 'Gray']
+    },
+    {
+      'text': 'Which is your favorite animal?',
+      'answer': ['Dog', 'Cat', 'Bird', 'Fish']
+    },
+    {
+      'text': 'Which is your favorite teacher?',
+      'answer': ['Eduardo', 'Laura', 'Paulo', 'Andreia']
+    }
+  ];
 
   _answer() {
-    setState(() {
-      _questionSelected++;
-    });
+    if (isThereSelectedQuestion) {
+      setState(() {
+        _questionSelected++;
+      });
+    }
+  }
 
-    print(_questionSelected);
+  bool get isThereSelectedQuestion {
+    final _condition = _questionSelected < _questions.length;
+    return _condition;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> questions = [
-      {
-        'text': 'Which is your favorite color?',
-        'answer': ['Red', 'Green', 'Blue', 'Gray']
-      },
-      {
-        'text': 'Which is your favorite animal?',
-        'answer': ['Dog', 'Cat', 'Bird', 'Fish']
-      },
-      {
-        'text': 'Which is your favorite teacher?',
-        'answer': ['Eduardo', 'Laura', 'Paulo', 'Andreia']
-      }
-    ];
-    List<String> responses = questions[_questionSelected]['answer'];
-    List<Widget> widgets =
-        responses.map((text) => Resposta(text, _answer)).toList();
+    List<String> responses = isThereSelectedQuestion
+        ? _questions[_questionSelected]['answer']
+        : null;
 
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
             title: Text('Questions'),
           ),
-          body: Column(
-            children: [
-              Questao(questions[_questionSelected]['text']),
-              ...widgets,
-            ],
-          )),
+          body: isThereSelectedQuestion
+              ? Column(
+                  children: [
+                    Questao(_questions[_questionSelected]['text']),
+                    ...responses
+                        .map((text) => Resposta(text, _answer))
+                        .toList(),
+                  ],
+                )
+              : null),
     );
   }
 }
